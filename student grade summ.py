cf81a83c964e08fe76a645e80dc1579381b42543
Zeezee <63296754+zzeeeezz@users.noreply.github.com>
2025-06-06 01:09:58 +0330

    Add files via upload

---
+++ student grade summ.py
@@ -0,0 +1,20 @@
+students_full_name = input("student's full name: ")
+print(students_full_name.title())
+student_age = eval(input("student's age: "))
+course_name = input("course name: ")
+ä,ö,ü = map(eval, input("three test scores: ").split())
+ß = ä + ö + ü
+print(f"Average score: {ß / 3}")
+    if ß / 3 >= 60:
+print("Passed, True")
+else:
+    print("Failed, False")
+
+
+
+
+
+
+
+
+
