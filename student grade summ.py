4267d85a43fbaaead9e2b8005663f7dd84fbd95b
Zeezee <63296754+zzeeeezz@users.noreply.github.com>
2025-06-06 01:14:32 +0330

    student grade summ.py

--- student grade summ.py
+++ student grade summ.py
@@ -5,8 +5,8 @@ course_name = input("course name: ")
 ä,ö,ü = map(eval, input("three test scores: ").split())
 ß = ä + ö + ü
 print(f"Average score: {ß / 3}")
-    if ß / 3 >= 60:
-print("Passed, True")
+if ß / 3 >= 60:
+    print("Passed, True")
 else:
     print("Failed, False")
 
